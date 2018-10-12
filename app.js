4e2f645a584ec36a2b12f07a4895c277160befa0
Jonathan Pinkard <jonathan.pinkard@gmail.com>
2018-10-11 21:09:41 -0500

    Added README, html, css, and js files for 99 Lines of Code Lab

---
+++ app.js
@@ -0,0 +1,15 @@
+let name = ['Ian', 'Chris', 'Jason', 'Demarcus', 'Adam']
+
+for (let i = 0; i <= 4; i++) {
+    console.log(name[i] + ':')
+
+    for (let j = 99; j > 1; j--) {
+        console.log(j + ' lines of code in the file, ' + j + ' lines of code; ' + name[i] + ' strikes one out, clears it all out, ' + (j - 1) + ' lines of code in the file')
+    }
+
+    if (j = 1) {
+        console.log(j + ' line of code in the file, ' + j + ' line of code; ' + name[i] + ' strikes one out, clears it all out, no more lines of code in the file')
+    }
+
+}
+
